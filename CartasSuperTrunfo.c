5608c1358f0c5e7021ce0bbdf1e4b9d3466147c9
kursk2629-dotcom <jonatassantos2629@gmail.com>
2025-09-16 20:09:02 +0000

    criei printf e váriaveis.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,45 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+//DECLARANDO AS VARIAVEIS
+    char estado[50];    
+    char cidade[50];
+    char carta[10];
+    double pib;   
+    int area, populacao, pontos;
   // Área para entrada de dados
+//COLETANDO AS CARACTERISTICAS
+    printf("\n---SUPER TRUNFO---\n");
+    printf("NOME DO ESTADO: ");
+    scanf(" %[^\n]", estado);
 
+    printf("NOME DA CIDADE: ");
+    scanf(" %[^\n]", cidade);
+
+    printf("CÓDIGO DA CARTA: ");
+    scanf("%s", carta);
+
+    printf("PIB: ");
+    scanf("%lf", &pib);  
+
+    printf("ÁREA: ");
+    scanf("%d", &area);
+
+    printf("POPULAÇÃO: ");
+    scanf("%d", &populacao);
+
+    printf("PONTOS TURÍSTICOS: ");
+    scanf("%d", &pontos);
   // Área para exibição dos dados da cidade
+//EXIBIDO AS INFORMACOES
+    printf("\n--- RESULTADO ---\n");
+    printf("NOME DO ESTADO: %s\n", estado);
+    printf("NOME DA CIDADE: %s\n", cidade);
+    printf("CÓDIGO DA CARTA: %s\n", carta);
+    printf("PIB: %.3lf\n", pib);   
+    printf("ÁREA: %dm²\n", area);
+    printf("POPULAÇÃO: %d\nPONTOS TURÍSTICOS: %d\n", populacao, pontos);
 
+    //RODADA DE DUAS CARTAS
 return 0;
 } 
